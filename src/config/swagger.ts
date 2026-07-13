2d527b28a95b40f025fe4b34fa70726f849c3785
fayzan101 <faizanbasheer38@gmail.com>
2026-07-14 00:48:17 +0500

    Update Swagger documentation title for consistency with application branding

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -32,7 +32,7 @@ export function setupSwagger(app: Application): void {
     `${apiPrefix}/docs`,
     swaggerUi.serve,
     swaggerUi.setup(swaggerDocument, {
-      customSiteTitle: "Anchor Heart API Docs",
+      customSiteTitle: "Anchor App API Docs",
       swaggerOptions: {
         persistAuthorization: true,
         docExpansion: "list",
